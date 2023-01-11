6be9b5bea85ae56b364c8729c49e5fd1abd8d05d
artmucha <arti.mucha@gmail.com>
2023-01-11 18:59:58 +0100

    add basic config

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pl_PL',
+    url: 'https://mysocial.pl',
+    site_name: 'MySocial',
+    title: 'MySocial',
+    images: [
+      {
+        url:
+          '/public/logo_black.svg',
+        width: 2560,
+        height: 1440,
+        alt: 'MySocial'
+      }
+    ]
+  },
+  twitter: {
+    handle: '@ArtiMucha',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,27 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
+import SEO from 'next-seo.config'
+
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <Head>
+      <title>MySocial</title>
+      <link rel="shortcut icon" href="/logo_black.svg" />
+      <link rel="apple-touch-icon" href="/logo_black.svg" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta
+        name="description"
+        content="MySocial"
+      />
+    </Head>
+    <DefaultSeo {...SEO} />
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
+
+export default App
